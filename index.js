38db7486c27bd8efb6eff18badcefaeaa831f567
46ra20 <rakib2046.md@gmail.com>
2022-10-25 05:25:17 -0400

    category file add in server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+port = 5000;
+
+const categoryData = require('./data/category.json');
+
+app.use(cors());
+
+app.get('/category',(req, res) =>{
+    res.send(categoryData)
+})
+
+app.listen(port, ()=>{
+    console.log(`Express is running in port ${port}`)
+})
